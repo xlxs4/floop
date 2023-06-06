06b2f028bb23720660ffc641d8f18a77cf5d52c8
xlxs4 <orousoultzoglou@gmail.com>
2023-06-06 13:36:17 +0300

    Add macroless quoted expressions

--- src/parsing.c
+++ src/parsing.c
@@ -31,7 +31,7 @@ typedef struct lval {
     struct lval** cell;
 } lval;
 
-enum { LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_SEXPR };
+enum { LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_SEXPR, LVAL_QEXPR };
 
 lval* lval_num(long x) {
     lval* v = malloc(sizeof(lval));
@@ -64,11 +64,21 @@ lval* lval_sexpr(void) {
     return v;
 }
 
+lval* lval_qexpr(void) {
+    lval* v = malloc(sizeof(lval));
+    v->type = LVAL_QEXPR;
+    v->count = 0;
+    v->cell = NULL;
+    return v;
+}
+
 void lval_del(lval* v) {
     switch (v->type) {
         case LVAL_NUM: break;
         case LVAL_ERR: free(v->err); break;
         case LVAL_SYM: free(v->sym); break;
+
+        case LVAL_QEXPR:
         case LVAL_SEXPR:
             for (int i = 0; i < v->count; i++) {
                 lval_del(v->cell[i]);
@@ -100,11 +110,17 @@ lval* lval_read(mpc_ast_t* t) {
     lval* x = NULL;
     if (strcmp(t->tag, ">") == 0) { x = lval_sexpr(); }
     if (strstr(t->tag, "sexpr")) { x = lval_sexpr(); }
+    if (strstr(t->tag, "qexpr")) { x = lval_qexpr(); }
 
     for (int i = 0; i < t->children_num; i++) {
         if (strcmp(t->children[i]->contents, "(") == 0) { continue; }
         if (strcmp(t->children[i]->contents, ")") == 0) { continue; }
+
+        if (strcmp(t->children[i]->contents, "}") == 0) { continue; }
+        if (strcmp(t->children[i]->contents, "{") == 0) { continue; }
+
         if (strcmp(t->children[i]->tag, "regex") == 0) { continue ; }
+
         x = lval_add(x, lval_read(t->children[i]));
     }
 
@@ -130,6 +146,7 @@ void lval_print(lval* v) {
         case LVAL_ERR: printf("Error: %s", v->err); break;
         case LVAL_SYM: printf("%s", v->sym); break;
         case LVAL_SEXPR: lval_expr_print(v, '(', ')'); break;
+        case LVAL_QEXPR: lval_expr_print(v, '{', '}'); break;
     }
 }
 
@@ -218,18 +235,20 @@ int main(int argc, char** argv) {
     mpc_parser_t* Number   = mpc_new("number");
     mpc_parser_t* Symbol   = mpc_new("symbol");
     mpc_parser_t* Sexpr    = mpc_new("sexpr");
+    mpc_parser_t* Qexpr    = mpc_new("qexpr");
     mpc_parser_t* Expr     = mpc_new("expr");
     mpc_parser_t* Floop    = mpc_new("floop");
 
     mpca_lang(MPCA_LANG_DEFAULT,
-    "                                        \
-    number : /-?[0-9]+/ ;                    \
-    symbol : '+' | '-' | '*' | '/' ;         \
-    sexpr  : '(' <expr>* ')' ;               \
-    expr   : <number> | <symbol> | <sexpr> ; \
-    floop  : /^/ <expr>* /$/ ;               \
+              "                                        \
+    number : /-?[0-9]+/ ;                              \
+    symbol : '+' | '-' | '*' | '/' ;                   \
+    sexpr  : '(' <expr>* ')' ;                         \
+    qexpr  : '{' <expr>* '}' ;                         \
+    expr   : <number> | <symbol> | <sexpr> | <qexpr> ; \
+    floop  : /^/ <expr>* /$/ ;                         \
     ",
-    Number, Symbol, Sexpr, Expr, Floop);
+    Number, Symbol, Sexpr, Qexpr, Expr, Floop);
 
     while (1) {
         char* input = readline("floop> ");
@@ -250,6 +269,6 @@ int main(int argc, char** argv) {
         free(input);
     }
 
-    mpc_cleanup(5, Number, Symbol, Sexpr, Expr, Floop);
+    mpc_cleanup(6, Number, Symbol, Sexpr, Qexpr, Expr, Floop);
     return 0;
 }
